b7a55bb8418ab98fa5a5d25fbef01fe86488e351
Aradhye Agarwal <aradhyeagarwal@gmail.com>
2024-02-04 22:12:35 +0530

    added checks for existing csv file

--- combine.py
+++ combine.py
@@ -3,8 +3,8 @@ import ast
 from tqdm import tqdm
 data = []
 data_rev = []
-df = pd.read_csv('data/submission_temp.csv') # loading training data
-df_rev = pd.read_csv('data/submission_temp_rev.csv') # loading training data
+df = pd.read_csv('data/submission.csv') # loading training data
+df_rev = pd.read_csv('data/submission_rev.csv') # loading training data
 ids = []
 for index, row in tqdm(df.iterrows()):
     data.append(ast.literal_eval(row['tagged_sentence']))# changing data-type of entries from 'str' to 'list'
@@ -17,5 +17,8 @@ for i in range(len(data)):
     n = len(data[i])
     submission['tagged_sentence'].append(data[i][:n//2]+data_rev[i][n//2:])
     submission['id'].append(ids[i])
-path_to_directory = 'data/'
-pd.DataFrame(submission).to_csv(path_to_directory +'final_submission_temp.csv', index = False)
+import os
+path = 'data/final.csv'
+if (os.path.exists(path)):
+    os.remove(path)
+pd.DataFrame(submission).to_csv(path, index = False)
